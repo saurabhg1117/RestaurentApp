ea0c19a3ec81f69533ccd0a5db056c577e9484f9
saurabhg1117 <saurabhg1117@gmail.com>
2026-03-19 02:06:06 +0530

    Corrected PrismaClient constructor options for version 7.5.0

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
 
 export default prisma;
